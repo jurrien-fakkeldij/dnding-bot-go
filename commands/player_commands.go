c2199b24f3852d63da8f4fe3c05f603e885f5ed3
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2024-08-13 23:18:30 +0200

    Added create player function for DM, needs tweaking

--- commands/player_commands.go
+++ commands/player_commands.go
@@ -20,11 +20,24 @@ var (
 					Type:        discordgo.ApplicationCommandOptionString,
 					Name:        "player_name",
 					Description: "Name of the player",
-					Required:    false,
+					Required:    true,
 				},
 			},
 		},
 		///TODO: ADD CREATE PLAYER
+		{
+			Name:         "create_player",
+			Description:  "[DM] Ability to register a player as a DM",
+			DMPermission: &dmPermission,
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "player_name",
+					Description: "Name of the player",
+					Required:    true,
+				},
+			},
+		},
 	}
 
 	PlayerCommandHandlers = map[string]CommandFunction{
@@ -73,12 +86,11 @@ var (
 						Flags:   discordgo.MessageFlagsEphemeral,
 					},
 				})
-
 				if err != nil {
 					return fmt.Errorf("[register-player] response error: %v -> %v", err, result.Error)
 				}
 
-				return fmt.Errorf("Could not save player [%s - %s]: %v", name, discordID, result.Error)
+				return fmt.Errorf("could not save player [%s - %s]: %v", name, discordID, result.Error)
 			}
 			err := session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -89,6 +101,82 @@ var (
 			})
 			if err != nil {
 				return fmt.Errorf("[register-player] response error: %v", err)
+			}
+
+			return nil
+		},
+		"create_player": func(session SessionModel, database *database.DB, logger *log.Logger, interaction *discordgo.InteractionCreate) error {
+			dm_role, err := HasMemberDMRole(session.(*discordgo.Session), interaction.Member, interaction.GuildID, logger)
+			if err != nil || !dm_role {
+				logger.Warn("Error or could not find dm_role", "error", err, "dm_role", dm_role, "user", interaction.Member.Nick)
+				_ = session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "You can not run this command, your are not recognized as a DM.",
+						Flags:   discordgo.MessageFlagsEphemeral,
+					},
+				})
+				return fmt.Errorf("could not find dm role or user doesn't have dm role")
+			}
+			options := interaction.ApplicationCommandData().Options
+			optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+			for _, opt := range options {
+				optionMap[opt.Name] = opt
+			}
+
+			name := ""
+			option, ok := optionMap["player_name"]
+			if ok {
+				name = option.StringValue()
+			} else {
+				name = interaction.Member.Nick
+			}
+
+			discordID := interaction.Member.User.ID
+
+			player := models.Player{Name: name, DiscordID: discordID}
+			var old_player []models.Player
+			database.Connection.Where("discord_id = ?", discordID).Find(&old_player)
+			if len(old_player) != 0 {
+				player = old_player[0]
+				logger.Warn("User is already registered.", "name", player.Name, "discordID", discordID, "new_name", name)
+				err := session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: fmt.Sprintf("You already registered %v. If this is not correct please contact the DM or admin", player.Name),
+						Flags:   discordgo.MessageFlagsEphemeral,
+					},
+				})
+				if err != nil {
+					return fmt.Errorf("[create-player] response error: %v", err)
+				}
+				return nil
+			}
+
+			result := database.GetConnection().Save(&player)
+			if result.Error != nil {
+				err := session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "Something went wrong, please try again or contact the server admin",
+						Flags:   discordgo.MessageFlagsEphemeral,
+					},
+				})
+				if err != nil {
+					return fmt.Errorf("[create-player] response error: %v -> %v", err, result.Error)
+				}
+
+				return fmt.Errorf("could not save player [%s - %s]: %v", name, discordID, result.Error)
+			}
+			err = session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("You have registered yourself with the name %s", name),
+					Flags:   discordgo.MessageFlagsEphemeral,
+				},
+			})
+			if err != nil {
+				return fmt.Errorf("[create-player] response error: %v", err)
 			}
 
 			return nil
